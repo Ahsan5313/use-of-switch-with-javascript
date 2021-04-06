d88069905c9d042483dd756ff5c2bccaa97c9717
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-06 14:28:11 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,38 @@
+var letter=prompt("Enter any letter : ");
+ letter=letter.toLowerCase();
+
+switch (letter){
+    case "a" :
+        document.write("The letter is : "+ "vowel");
+        break;
+        case "e":
+            document.write("The letter is : "+ "vowel");
+            break;
+            case "i":
+            document.write("The letter is : "+ "vowel");
+            break;
+            case "o":
+            document.write("The letter is : "+ "vowel");
+            break;
+            case "u":
+            document.write("The letter is : "+ "vowel");
+            break;
+            case "l":
+            document.write("The letter is : "+ "vowel");
+            break;
+            default:
+            document.write("The letter is : "+ "consonent");
+           
+
+}
+
+
+
+
+
+
+
+
+
+
+
